fe72a8b7be8f5f9cbc8dae611747ee07e288037d
RidaShaikh304 <85176059+RidaShaikh@users.noreply.github.com>
2023-04-11 21:04:02 +0500

    Update and rename Q2.html to Q2.js

---
+++ Assignment 2/Q2.js
@@ -0,0 +1,10 @@
+let arr = [23, 34, 60, 70, 90, 34, 12, 20];
+    function findNumber(i) {
+        if (arr = arr.includes(i)) {
+            document.write(i + " is found in array");
+        }
+        else {
+            document.write(i + " is not found in array");
+        }
+    }
+findNumber(30);
